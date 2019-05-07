635840da4fffa04edd8fbf164a6fe0d970e574a1
Luflosi <luflosi@luflosi.de>
2019-05-07 21:10:50 +0200

    Don't try to remove compile_commands.json

--- setup.py
+++ setup.py
@@ -859,7 +859,7 @@ def clean():
                 else:
                     os.unlink(x)
 
-    safe_remove('build', 'compile_commands.json', 'linux-package', 'kitty.app')
+    safe_remove('build', 'linux-package', 'kitty.app')
     for root, dirs, files in os.walk('.'):
         remove_dirs = {d for d in dirs if d == '__pycache__'}
         [(shutil.rmtree(os.path.join(root, d)), dirs.remove(d)) for d in remove_dirs]
